280714228c4e25e7222f78a15aaf31652a24c6f1
Chetna Mittal <chetna.mittal125@gmail.com>
2025-01-14 12:12:37 +0530

    chore: update dependencies and setup of the application

--- config/settings.go
+++ config/settings.go
@@ -19,13 +19,17 @@ type Settings struct {
 	SlackReportingUrl           string
 	DataMarketAddresses         []string
 	DataMarketContractAddresses []common.Address
-	RedisDb                     int
+	RedisDB                     int
+	BlockTime                   int
+	SlotSyncInterval            int
+	PollingStaticStateVariables bool
 }
 
 func LoadConfig() {
 	dataMarketAddresses := getEnv("DATA_MARKET_ADDRESSES", "[]")
 	dataMarketAddressesList := []string{}
 	dataMarketContractAddresses := []common.Address{}
+
 	err := json.Unmarshal([]byte(dataMarketAddresses), &dataMarketAddressesList)
 	if err != nil {
 		log.Fatalf("Failed to parse DATA_MARKET_ADDRESSES environment variable: %v", err)
@@ -33,9 +37,21 @@ func LoadConfig() {
 	if len(dataMarketAddressesList) == 0 {
 		log.Fatalf("DATA_MARKET_ADDRESSES environment variable has an empty array")
 	}
-	for _, addr := range dataMarketAddressesList {
-		dataMarketContractAddresses = append(dataMarketContractAddresses, common.HexToAddress(addr))
+
+	for _, dataMarketAddress := range dataMarketAddressesList {
+		dataMarketContractAddresses = append(dataMarketContractAddresses, common.HexToAddress(dataMarketAddress))
 	}
+
+	pollingStaticStateVariables, pollingStaticStateVariablesErr := strconv.ParseBool(getEnv("POLLING_STATIC_STATE_VARIABLES", "true"))
+	if pollingStaticStateVariablesErr != nil {
+		log.Fatalf("Failed to parse POLLING_STATIC_STATE_VARIABLES environment variable: %v", pollingStaticStateVariablesErr)
+	}
+
+	slotSyncInterval, err := strconv.Atoi(getEnv("SLOT_SYNC_INTERVAL", "60"))
+	if err != nil {
+		log.Fatalf("Invalid SLOT_SYNC_INTERVAL value: %v", err)
+	}
+
 	config := Settings{
 		ClientUrl:                   getEnv("PROST_RPC_URL", ""),
 		ContractAddress:             getEnv("PROTOCOL_STATE_CONTRACT", ""),
@@ -44,41 +60,21 @@ func LoadConfig() {
 		SlackReportingUrl:           getEnv("SLACK_REPORTING_URL", ""),
 		DataMarketAddresses:         dataMarketAddressesList,
 		DataMarketContractAddresses: dataMarketContractAddresses,
+		SlotSyncInterval:            slotSyncInterval,
+		PollingStaticStateVariables: pollingStaticStateVariables,
 	}
 
-	// Check for any missing required environment variables and log errors
-	missingEnvVars := []string{}
-	if config.ClientUrl == "" {
-		missingEnvVars = append(missingEnvVars, "PROST_RPC_URL")
+	redisDB, redisDBParseErr := strconv.Atoi(getEnv("REDIS_DB", ""))
+	if redisDBParseErr != nil || redisDB < 0 {
+		log.Fatalf("Failed to parse REDIS_DB environment variable: %v", redisDBParseErr)
 	}
-	if config.ContractAddress == "" {
-		missingEnvVars = append(missingEnvVars, "PROTOCOL_STATE_CONTRACT")
-	}
-	if len(config.DataMarketAddresses) == 0 {
-		missingEnvVars = append(missingEnvVars, "DATA_MARKET_ADDRESSES")
-	}
-	if getEnv("REDIS_DB", "") == "" {
-		missingEnvVars = append(missingEnvVars, "REDIS_DB")
-	}
-	if config.RedisHost == "" {
-		missingEnvVars = append(missingEnvVars, "REDIS_HOST")
-	}
-	if config.RedisPort == "" {
-		missingEnvVars = append(missingEnvVars, "REDIS_PORT")
-	}
+	config.RedisDB = redisDB
 
-	if len(missingEnvVars) > 0 {
-		log.Fatalf("Missing required environment variables: %v", missingEnvVars)
+	blockTime, blockTimeParseErr := strconv.Atoi(getEnv("BLOCK_TIME", ""))
+	if blockTimeParseErr != nil {
+		log.Fatalf("Failed to parse BLOCK_TIME environment variable: %v", blockTimeParseErr)
 	}
-
-	redisDb, err := strconv.Atoi(getEnv("REDIS_DB", ""))
-	if err != nil || redisDb < 0 {
-		log.Fatalf("Invalid REDIS_DB value: %v", redisDb)
-	}
-
-	config.RedisDb = redisDb
-
-	checkOptionalEnvVar(config.SlackReportingUrl, "SLACK_REPORTING_URL")
+	config.BlockTime = blockTime
 
 	SettingsObj = &config
 }
@@ -89,10 +85,4 @@ func getEnv(key, defaultValue string) string {
 		return defaultValue
 	}
 	return value
-}
-
-func checkOptionalEnvVar(value, key string) {
-	if value == "" {
-		log.Warnf("Optional environment variable %s is not set", key)
-	}
 }
